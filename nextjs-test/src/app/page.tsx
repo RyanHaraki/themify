ba4d802e537200082cff4cac1c24105e22f07394
Mukund Mauji <mauji.mukund@yahoo.ca>
2025-03-02 20:44:30 -0500

    mukund fixes things

--- nextjs-test/src/app/page.tsx
+++ nextjs-test/src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return <div>hey there</div>;
+  return (
+    <div>
+      hey there
+      <br /> go to <a href="/dashboard"> dashboard </a>{' '}
+    </div>
+  );
 }
